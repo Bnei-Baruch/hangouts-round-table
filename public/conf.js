839655694299f3c5b6c573341ad33935ce6f3c97
root <root@zulu765-119.local>
2012-12-11 19:52:57 +0300

    update set interval + conf

--- public/conf.js
+++ public/conf.js
@@ -1,4 +1,6 @@
 var conf = {
   'host': 'http://bbworkshop.kbb1.com/public',
-  'webdis': 'http://bbworkshop.kbb1.com/'
+  'webdis': 'http://bbworkshop.kbb1.com',
+  'hangout_upadate_timeout': 3000, // in milliseconds.
+  'button_update_interval': 5000   // in milliseconds.
 }
