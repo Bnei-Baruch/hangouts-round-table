870260167064958738c6dde403e2514d5f5c8b45
Konstantin Alexandrov <negus@negus-desktop.socio2.net>
2014-12-04 05:23:21 +0100

    Debugging server script added

---
+++ frontend/gulp/server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var browserSync = require('browser-sync');
+
+function browserSyncInit(baseDir, files, browser) {
+  browser = browser === undefined ? 'default' : browser;
+
+  browserSync.instance = browserSync.init(files, {
+    startPath: '/',
+    server: {
+      baseDir: baseDir,
+      directory: true
+    },
+    browser: browser
+  });
+
+}
+
+gulp.task('serve', ['watch'], function () {
+  browserSyncInit([
+    'src',
+    '.tmp'
+  ], [
+    '.tmp/{app,components}/**/*.css',
+    'src/assets/images/**/*',
+    'src/*.html',
+    'src/{app,components}/**/*.html',
+    'src/{app,components}/**/*.js'
+  ]);
+});
+
+gulp.task('serve:dist', ['build'], function () {
+  browserSyncInit('dist');
+});
