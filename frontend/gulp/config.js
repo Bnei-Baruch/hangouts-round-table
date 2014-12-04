7d5a4ad7eb34ade325d85208328cec397917e4ed
Konstantin Alexandrov <koalexan@microsoft.com>
2014-12-04 10:35:41 +0100

    App config component fully implemented

--- frontend/gulp/config.js
+++ frontend/gulp/config.js
@@ -6,10 +6,12 @@ var $ = require('gulp-load-plugins')();
 
 /* Generate config component from template */
 gulp.task('config', function () {
+  var config = require('../config.json', true);
+
   gulp.src('src/components/app-config/app-config.template.html')
     .pipe($.rename('app-config.html'))
     .pipe($.data(function () {
-      return {settings: require('../config.json')};
+      return {config: JSON.stringify(config, null, 2)};
     }))
     .pipe($.template())
     .pipe(gulp.dest('src/components/app-config'));
