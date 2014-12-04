f90f66d840f019bed76905efbf8767026cef6a90
Konstantin Alexandrov <koalexan@microsoft.com>
2014-12-04 12:56:17 +0100

    Table list widget refactored

--- frontend/src/components/table-list/table-list.js
+++ frontend/src/components/table-list/table-list.js
@@ -5,8 +5,6 @@
     publish: {
       space: 'default'
     },
-    created: function () {
-    },
     ready: function() {
       var that = this;
       function refreshTables() {
@@ -14,8 +12,6 @@
       }
       refreshTables();
       setInterval(refreshTables, 10000);
-    },
-    select: function () {
     },
     setTables: function (e) {
       var that = this;
