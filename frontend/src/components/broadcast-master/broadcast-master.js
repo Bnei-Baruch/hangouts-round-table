ae0ece2e668dc7a0c81ac2aff402873b16a36796
Konstantin Alexandrov <negus@negus-laptop>
2014-12-13 19:05:30 +0100

    Mute/start button behaviour fixed

--- frontend/src/components/broadcast-master/broadcast-master.js
+++ frontend/src/components/broadcast-master/broadcast-master.js
@@ -10,11 +10,16 @@
 
       this.webRtcPeer = kurentoUtils.WebRtcPeer.startSendOnly(
         this.$.localVideo, function (sdpOffer) {
+          console.log('Media accepted');
+
           kurentoClient(that.$.config.kurentoWsUri, that.cancelOnError(function(error, kurentoClient) {
+            console.log('Client initialized');
 
             kurentoClient.create('MediaPipeline', that.cancelOnError(function(error, pipeline) {
+              console.log('Pipeline created');
 
               pipeline.create('WebRtcEndpoint', that.cancelOnError(function(error, webRtc){
+                console.log('Endpoint created');
                 that.sendMessage({action: 'registerMaster', endpointId: webRtc.id});
 
                 webRtc.processOffer(sdpOffer, that.cancelOnError(function(error, sdpAnswer){
@@ -28,7 +33,7 @@
         }, this.onError);
     },
     isOnHoldChanged: function () {
-      this.toggleBroadcast(this.isOnHold);
+      this.toggleBroadcast(!this.isOnHold);
     },
     isMutedChanged: function () {
       var audioTracks = this.webRtcPeer.stream.getAudioTracks();
@@ -39,8 +44,8 @@
       for (var trackIndex in tracks) {
         var track = tracks[trackIndex];
 
-        if (track.kind === 'audio' && enabled) {
-          track.enabled = !this.isMuted;
+        if (track.kind === 'audio') {
+          track.enabled = !this.isMuted && enabled;
         } else {
           track.enabled = enabled;
         }
