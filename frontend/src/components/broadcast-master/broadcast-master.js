745b555c6821f1aabb9d7ab62421cd16ae944b36
Konstantin Alexandrov <negus@negus-desktop.socio2.net>
2014-12-13 19:35:13 +0100

    Resume/Mute button behaviour fixed

--- frontend/src/components/broadcast-master/broadcast-master.js
+++ frontend/src/components/broadcast-master/broadcast-master.js
@@ -4,26 +4,22 @@
   Polymer({
     isReady: false,
     isMuted: false,
-    isOnHold: false,
+    isEnabled: false,
     initKurento: function () {
       var that = this;
 
       this.webRtcPeer = kurentoUtils.WebRtcPeer.startSendOnly(
         this.$.localVideo, function (sdpOffer) {
-          console.log('Media accepted');
 
           kurentoClient(that.$.config.kurentoWsUri, that.cancelOnError(function(error, kurentoClient) {
-            console.log('Client initialized');
 
             kurentoClient.create('MediaPipeline', that.cancelOnError(function(error, pipeline) {
-              console.log('Pipeline created');
 
               pipeline.create('WebRtcEndpoint', that.cancelOnError(function(error, webRtc){
-                console.log('Endpoint created');
                 that.sendMessage({action: 'registerMaster', endpointId: webRtc.id});
 
                 webRtc.processOffer(sdpOffer, that.cancelOnError(function(error, sdpAnswer){
-                  that.toggleBroadcast(false);
+                  that.toggleBroadcast();
                   that.webRtcPeer.processSdpAnswer(sdpAnswer);
                   that.isReady = true;
                 }));
@@ -32,22 +28,21 @@
           }));
         }, this.onError);
     },
-    isOnHoldChanged: function () {
-      this.toggleBroadcast(!this.isOnHold);
+    isEnabledChanged: function () {
+      this.toggleBroadcast();
     },
     isMutedChanged: function () {
-      var audioTracks = this.webRtcPeer.stream.getAudioTracks();
-      audioTracks[0].enabled = !(this.isMuted || this.isOnHold);
+      this.toggleBroadcast(true);
     },
-    toggleBroadcast: function (enabled) {
+    toggleBroadcast: function (onlyAudio) {
       var tracks = this.webRtcPeer.stream.getTracks();
       for (var trackIndex in tracks) {
         var track = tracks[trackIndex];
 
         if (track.kind === 'audio') {
-          track.enabled = !this.isMuted && enabled;
-        } else {
-          track.enabled = enabled;
+          track.enabled = !this.isMuted && this.isEnabled;
+        } else if (!onlyAudio) {
+          track.enabled = this.isEnabled;
         }
       }
     }
