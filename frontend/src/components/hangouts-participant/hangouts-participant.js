baa8a174f8f502e09edd3516be058fdbed82129f
Konstantin Alexandrov <negus@negus-desktop.socio2.net>
2014-12-18 04:35:43 +0100

    Self mute added

--- frontend/src/components/hangouts-participant/hangouts-participant.js
+++ frontend/src/components/hangouts-participant/hangouts-participant.js
@@ -1,13 +1,39 @@
 'use strict';
 
 (function () {
-  Polymer({
-    onMasterResumedMessage: function () {
 
+  var runIfHangoutsReady = function(func) {
+    var that = this;
+
+    return function () {
+      if (that.isHangoutsApiReady) {
+        func.apply(this, arguments);
+      } else {
+        console.debug("Hangouts API is not ready yet");
+      }
+    };
+  };
+
+  Polymer({
+    isHangoutsApiReady: false,
+    created: function () {
+      var that = this;
+
+      var _initHangouts = function (apiInitEvent) {
+        if (apiInitEvent.isApiReady) {
+          that.isHangoutsApiReady = true;
+          gapi.hangout.onApiReady.remove(_initHangouts);
+        }
+      };
+
+      gapi.hangout.onApiReady.add(_initHangouts);
     },
-    onMasterPausedMessage: function () {
+    onMasterResumedMessage: runIfHangoutsReady(function () {
+      gapi.hangout.av.setMicrophoneMute(true);
+    }),
+    onMasterPausedMessage: runIfHangoutsReady(function () {
       gapi.hangout.hideApp();
-    },
+    }),
   });
 
 })();
