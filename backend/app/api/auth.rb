7a31bbe034ca3c0315857d67c7b79c2ea01f9613
Konstantin Alexandrov <koalexan@microsoft.com>
2014-12-25 12:56:49 +0100

    Auth token creation refactored

--- backend/app/api/auth.rb
+++ backend/app/api/auth.rb
@@ -4,28 +4,34 @@ class RoundTable::API
     body = JSON.parse(request.body.read)
     user_json = redis.get("auth_user_#{body['user']}")
 
-    auth_error = { :error => "Invalid user name or password" }
-    bad_request_response = [400, [auth_error.to_json]]
-
     if user_json.nil?
-      bad_request_response
+      response_bad_request
     else
       user = JSON.parse(user_json)
 
       if BCrypt::Password.new(user['password']) == body['password']
-        token = SecureRandom.urlsafe_base64
-        key = "auth_session_#{token}"
-        redis.set(key, true)
-        redis.expire(key, config['auth']['session_ttl'])
-
         status 201
         {
-          :token => token,
+          :token => create_auth_token,
           :space => user['space']
         }.to_json
       else
-        bad_request_response
+        response_bad_request
       end
     end
   end
+
+  def create_auth_token
+    token = SecureRandom.urlsafe_base64
+    key = "auth_session_#{token}"
+    redis.set(key, true)
+    redis.expire(key, config['auth']['session_ttl'])
+    token
+  end
+
+  def response_bad_request
+    auth_error = { :error => "Invalid user name or password" }
+    [400, [auth_error.to_json]]
+  end
+
 end
