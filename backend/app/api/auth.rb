3468abbde584baaadf021ffa09eea071496fae3d
Konstantin Alexandrov <negus@negus-desktop.socio2.net>
2014-12-27 06:23:42 +0100

    Basic auth test fixed

--- backend/app/api/auth.rb
+++ backend/app/api/auth.rb
@@ -25,28 +25,6 @@ class RoundTable::API
     end
   end
 
-  # Create auth token
-  post '/auth/tokens' do
-    body = JSON.parse(request.body.read)
-    user_json = redis.get("auth_user_#{body['user']}")
-
-    if user_json.nil?
-      response_auth_error
-    else
-      user = JSON.parse(user_json)
-
-      if BCrypt::Password.new(user['password']) == body['password']
-        status 201
-        {
-          :token => create_auth_token,
-          :space => user['space']
-        }.to_json
-      else
-        response_auth_error
-      end
-    end
-  end
-
   def create_auth_token
     token = SecureRandom.urlsafe_base64
     key = "auth_session_#{token}"
--- backend/spec/api/auth_spec.rb
+++ backend/spec/api/auth_spec.rb
@@ -1,3 +1,4 @@
+require 'base64'
 require_relative '../spec_helper'
 
 describe RoundTable::API do
@@ -6,25 +7,23 @@ describe RoundTable::API do
     create_sample_user
   end
 
+  it "should ask for authentication" do
+    expect_ask_for_auth
+
+    auth_header("user", "invalid")
+    expect_ask_for_auth
+  end
+
   it "should authenticate a user" do
-    body = {
-      :user => "invalid",
-      :password => "user"
-    }.to_json
+    auth_header("user", "swordfish")
 
-    post '/auth/tokens', body
-    expect(last_response).to be_bad_request
-
-    body = {
-      :user => "user",
-      :password => "swordfish"
-    }.to_json
-
-    post '/auth/tokens', body
+    get '/auth/basic'
     expect(last_response).to be_successful
+
     json_response = JSON.parse(last_response.body)
     expect(json_response).to include('token')
     expect(json_response['token'].empty?).to be_falsy
+    expect(json_response['space']).to eq("default")
   end
 
   def create_sample_user
@@ -36,4 +35,14 @@ describe RoundTable::API do
     redis.set('auth_user_user', sample_user)
   end
 
+  def expect_ask_for_auth
+    get '/auth/basic'
+    expect(last_response.status).to eq(401)
+    expect(last_response.header).to include("WWW-Authenticate")
+  end
+
+  def auth_header(login, password)
+    encoded = Base64.encode64("#{login}:#{password}")
+    header "Authorization", "Basic #{encoded}"
+  end
 end
