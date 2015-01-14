885b6738428ac852335572554d4ba385a42d7c28
Konstantin Alexandrov <negus@negus-desktop.socio2.net>
2015-01-14 05:38:23 +0100

    Dashboard template updated

--- frontend/src/components/heartbeat-dashboard/heartbeat-dashboard.js
+++ frontend/src/components/heartbeat-dashboard/heartbeat-dashboard.js
@@ -2,5 +2,20 @@
 
 (function () {
   Polymer({
+    maxParticipantsNumber: 0,
+    create: function () {
+      this.tables = {};
+    },
+    update: function (e, message) {
+      if (message.participants.length > this.maxParticipantsNumber) {
+        this.maxParticipantsNumber = message.participants.length;
+      }
+
+      if (!(message.tableId in this.tables)) {
+        this.tables[message.tableId] = {
+          participants:4
+        }
+      }
+    }
   });
 })();
