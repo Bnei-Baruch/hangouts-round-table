348d105bda4bbb6eb466095874db4b6c66e8bff8
Konstantin Alexandrov <koalexan@microsoft.com>
2015-01-15 14:08:12 +0100

    Made signaling a singleton

--- frontend/src/components/backend-signaling/backend-signaling.js
+++ frontend/src/components/backend-signaling/backend-signaling.js
@@ -1,27 +1,31 @@
 'use strict';
 
 (function () {
+  var backendWs;
+
   Polymer({
     ready: function() {
-      this.initBackendSocket();
+      if (backendWs === undefined) {
+        this.initBackendSocket();
+      }
     },
     initBackendSocket: function () {
       var that = this;
 
-      this.backendWs = new WebSocket(this.$.config.backendWsUri);
+      backendWs = new WebSocket(this.$.config.backendWsUri);
 
-      this.backendWs.onopen = function () {
+      backendWs.onopen = function () {
         that.fire('websocket-open');
       };
 
-      this.backendWs.onmessage = function (message) {
+      backendWs.onmessage = function (message) {
         var parsedMessage = JSON.parse(message.data);
 
         console.debug("New message from the backend:", parsedMessage);
         that.fire(parsedMessage.action, parsedMessage);
       };
 
-      this.backendWs.onclose = function () {
+      backendWs.onclose = function () {
         window.setTimeout(function () {
           console.log("Reconnecting to the backend...");
           that.initBackendSocket();
@@ -31,7 +35,7 @@
     sendMessage: function (message) {
       message.space = this.space;
       var jsonMessage = JSON.stringify(message);
-      this.backendWs.send(jsonMessage);
+      backendWs.send(jsonMessage);
     }
   });
 })();
