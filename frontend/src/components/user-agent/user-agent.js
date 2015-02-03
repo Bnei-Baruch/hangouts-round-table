261cdb690b11a85fa5b5da0c30bb574f27674c38
bbfsdev <bbfsdev@gmail.com>
2015-02-02 22:57:49 -0500

    Layout fixes applied, bad user agent page refactored

--- frontend/src/components/user-agent/user-agent.js
+++ frontend/src/components/user-agent/user-agent.js
@@ -2,25 +2,10 @@
 
 (function () {
   Polymer({
-    publish: {
-      show: "message"
-    },
-    getParser: function() {
-      var that = this;
-      if (!that.parser) {
-        that.parser = new UAParser();
-        console.log(that.parser.getResult());
+    ready: function() {
+      for (var key in window.bowser) {
+        this[key] = window.bowser[key];
       }
-      return that.parser;
-    },
-    getUserAgentMessage: function() {
-      var that = this;
-      var message = "You are using ";
-      message += that.getParser().getBrowser().name + " " + that.getParser().getBrowser().major + " browser";
-      if (that.getParser().getOS().name) {
-        message += " on " + that.getParser().getOS().name + " " + that.getParser().getOS().version;
-      }
-      return message + ".";
     }
   });
 
