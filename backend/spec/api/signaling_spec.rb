8fcc5ea7b3c9cedc0a3e8cc50d3c78fced6591cd
bbfsdev <bbfsdev@gmail.com>
2015-02-19 04:34:55 -0500

    Websocket tests framework implemented initially

--- backend/spec/api/signaling_spec.rb
+++ backend/spec/api/signaling_spec.rb
@@ -6,4 +6,29 @@ describe RoundTable::API do
     expect(last_response).to be_bad_request
   end
 
+  it "should register endpoint for a specific role" do
+    send_ws_message("kuku")
+  end
+
+  def send_ws_message(msg)
+    app.helpers.request.stub(:websocket?).and_return(true)
+    app.helpers.request.stub(:websocket).and_return(WebsocketMock.new(msg))
+    get '/socket'
+  end
+
+  class WebsocketMock
+
+    def initialize(msg)
+      @msg = msg
+    end
+
+    def send
+    end
+
+    def onmessage
+    end
+
+    def onclose
+    end
+  end
 end
