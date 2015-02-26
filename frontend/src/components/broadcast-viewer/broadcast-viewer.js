8a1623dc1c61a3821b14067814c6cf81b26632bd
bbfsdev <bbfsdev@gmail.com>
2015-02-25 22:54:53 -0500

    Lazy translator endpoint connection implemented initially

--- frontend/src/components/broadcast-viewer/broadcast-viewer.js
+++ frontend/src/components/broadcast-viewer/broadcast-viewer.js
@@ -4,9 +4,14 @@
   Polymer({
     instructorEndpointId: null,
     assignMasterEndpoints: function (e, message) {
-      this.instructorEndpointId = message.instructorEndpointId;
-      this.translatorEndpointId = message.translatorEndpointId;
-      this.initKurento();
+      if (message.instructorEndpointId !== this.instructorEndpointId) {
+        this.instructorEndpointId = message.instructorEndpointId;
+        this.initKurento();
+      }
+      if (message.translatorEndpointId !== this.translatorEndpointId) {
+        this.translatorEndpointId = message.translatorEndpointId;
+        this.connectTranslatorEndpoint();
+      }
     },
     register: function () {
       this.$.signaling.sendMessage({action: 'register-viewer'});
@@ -19,6 +24,7 @@
       this.webRtcPeer = kurentoUtils.WebRtcPeer.startRecvOnly(
         that.$.remoteVideo, function (sdpOffer) {
           kurentoClient(that.$.config.kurentoWsUri, that.cancelOnError(function(error, kurentoClient) {
+            that.kurentoClient = kurentoClient;
 
             kurentoClient.getMediaobjectById(that.instructorEndpointId, that.cancelOnError(function(error, instructorEndpoint) {
 
@@ -34,11 +40,9 @@
                       console.log('Connected to the instructor');
                     }));
 
-                    kurentoClient.getMediaobjectById(that.translatorEndpointId, that.cancelOnError(function(error, translatorEndpoint) {
-                      translatorEndpoint.connect(viewerEndpoint, that.cancelOnError(function(){
-                        console.log('Connected to the translator');
-                      }));
-                    }));
+                    if (that.translatorEndpointId) {
+                      that.connectTranslatorEndpoint();
+                    }
 
                     viewerEndpoint.processOffer(sdpOffer, that.cancelOnError(function(error, sdpAnswer){
                       that.webRtcPeer.processSdpAnswer(sdpAnswer);
@@ -50,6 +54,15 @@
             }));
           }));
         }, this.onError);
+    },
+    connectTranslatorEndpoint: function () {
+      var that = this;
+
+      this.kurentoClient.getMediaobjectById(this.translatorEndpointId, this.cancelOnError(function(error, translatorEndpoint) {
+        translatorEndpoint.connect(that.viewerEndpoint, that.cancelOnError(function(){
+          console.log('Connected to the translator');
+        }));
+      }));
     }
   });
 
