0d38028b322717cdba6adab0e1e0fd3e1cd97be8
bbfsdev <bbfsdev@gmail.com>
2015-03-04 08:59:23 -0500

    Hangout IDs generator implemented

---
+++ backend/scripts/generate_hangout_ids.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+CONSTS_FILE="../consts.yaml"
+
+echo "Generating Hangout IDs"
+
+echo "---\nhangout_ids:" > "$CONSTS_FILE"
+
+for i in `seq 1 50`;
+do
+  wget -x --load-cookies hangouts-cookies.txt https://plus.google.com/hangouts/_/ -O .hangouts.html
+  hangout_id=$(grep -Po '(?<=https:\/\/plus.google.com\/hangouts\/_\/)g\w*' .hangouts.html);
+
+  if [ ! -z "${hangout_id}" ]
+  then
+    echo "  - ${hangout_id}" >> "$CONSTS_FILE"
+  fi
+done   
