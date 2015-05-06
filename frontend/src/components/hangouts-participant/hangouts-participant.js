e2e652cea84012e6179578fc7aae4013492c251b
kolmanv <kolmanv@gmail.com>
2015-05-05 22:01:36 -0400

    Users numbers implemented

--- frontend/src/components/hangouts-participant/hangouts-participant.js
+++ frontend/src/components/hangouts-participant/hangouts-participant.js
@@ -10,25 +10,47 @@
           gapi.hangout.layout.setChatPaneVisible(false);
           that.initOverlay();
           gapi.hangout.onApiReady.remove(initHangouts);
+          gapi.hangout.av.setLocalParticipantVideoMirrored(false);
         }
       };
       gapi.hangout.onApiReady.add(initHangouts);
+
+      var initOverlay = function () {
+        that.initOverlay();
+      };
+
+      gapi.hangout.onEnabledParticipantsChanged.add(initOverlay);
     },
     initOverlay: function () {
       var participantNumber = this.getParticipantNumber();
 
+      this.releaseOverlay();
+
       if (participantNumber > 0) {
         var imagePath = this.$.config.numbersImagePath.replace('{}', participantNumber);
 
-        var resource = gapi.hangout.av.effects.createImageResource(
+        this.numberResource = gapi.hangout.av.effects.createImageResource(
           this.$.config.frontendUrl + imagePath);
 
-        resource.showOverlay({
+        this.numberOverlay = this.numberResource.showOverlay({
           position: {
             x: 0.0,
-            y: 0.4
+            y: 0.3
+          },
+          scale: {
+            magnitude: 0.2,
+            reference: gapi.hangout.av.effects.ScaleReference.WIDTH
           }
         });
+      }
+    },
+    releaseOverlay: function () {
+      if (this.numberResource) {
+        this.numberResource.dispose();
+      }
+
+      if (this.numberOverlay) {
+        this.numberOverlay.dispose();
       }
     },
     mute: function () {
@@ -46,6 +68,8 @@
       hangoutParticipants.forEach(function (participant) {
         participantIds.push(participant.id);
       });
+
+      participantIds.sort();
 
       return participantIds.indexOf(localParticipantId) + 1;
     }
