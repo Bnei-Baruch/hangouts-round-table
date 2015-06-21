da74ec4dfd5c32ab05df9289f1dc87d1c2defb6d
kolmanv <kolmanv@gmail.com>
2015-06-21 10:55:42 -0400

    New events loading fixed

--- frontend/src/components/onair-player/onair-player.js
+++ frontend/src/components/onair-player/onair-player.js
@@ -1,75 +1,110 @@
 'use strict';
 
-// this is test
-var rt_config, rt_interval;
+(function () {
 
-function rt_pollEventsApi() {
-  httpGet(rt_config.liveIdUrl, function(ret) {
-    if (ret.id) {
-      var checkIsLiveUrl = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + ret.id + "&key=AIzaSyBoMXQDrlRUCQCxv4fjfiyTHXog8OB2Nz0";
-      httpGet(checkIsLiveUrl, function (data) {
-        if (data.items && data.items.length > 0 && data.items[0].snippet.liveBroadcastContent !== "none") {
-          window.clearInterval(rt_interval);
-          rt_loadYouTubePlayer(ret.id);
-        }
-      });
-    }
-  });
-}
+  var config, pollingInterval,
+    lastLiveId = null, splashElement;
 
-function httpGet(theUrl, callback) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState === 4) {
-      callback(JSON.parse(xmlHttp.responseText));
-    }
+  /*
+   * config:
+   *  channelId
+   *  callback
+   *  width
+   *  height
+   */
+  window.initOnAirPlayer = function (cfg) {
+    config = cfg;
+    splashElement = document.getElementById(config.containerId).cloneNode();
+    loadYouTubeAPI();
   };
-  xmlHttp.open("GET", theUrl, true);
-  xmlHttp.send();
-}
 
-/*
- * config:
- * channelId
- * callback
- * width
- * height
- */
-function initOnAirPlayer(config) {
-  rt_config = config;
-  rt_loadYouTubeAPI();
-}
+  window.onYouTubeIframeAPIReady = function () {
+    pollingInterval = window.setInterval(pollEventsApi, 5000);
+    pollEventsApi();
+  };
 
-function onYouTubeIframeAPIReady() {
-  rt_interval = window.setInterval(rt_pollEventsApi, 5000);
-  rt_pollEventsApi();
-}
 
-function rt_loadYouTubePlayer(liveId) {
-  var player = new YT.Player(rt_config.containerId, {
-    width: rt_config.width,
-    height: rt_config.height,
-    videoId: liveId,
-    playerVars : {
-      version: 3,
-      autoplay : 1
-    }
-  });
+  ////
 
-  var getTitleUrl = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + liveId + "&key=AIzaSyBoMXQDrlRUCQCxv4fjfiyTHXog8OB2Nz0";
-  httpGet(getTitleUrl, function(data) {
-    var title = "";
-    if (data.items && data.items.length > 0) {
-      title = data.items[0].snippet.title;
-    }
-    rt_config.callback(title, player);
-  });
-}
+  function showSplash() {
+    var container = document.getElementById(config.containerId);
+    var body = container.parentNode;
+    body.removeChild(container);
+    var splashClone = splashElement.cloneNode();
+    body.appendChild(splashClone);
+  }
 
-function rt_loadYouTubeAPI() {
-  var tag = document.createElement('script');
+  function loadYouTubePlayer(liveId) {
+    console.debug('loadYouTubePlayer ' + liveId);
+    var player = new YT.Player(config.containerId, {
+      width: config.width,
+      height: config.height,
+      videoId: liveId,
+      playerVars : {
+        version: 3,
+        autoplay : 1
+      }
+    });
 
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-}
+    var getTitleUrl = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + liveId + "&key=AIzaSyBoMXQDrlRUCQCxv4fjfiyTHXog8OB2Nz0";
+    httpGet(getTitleUrl, function(data) {
+      var title = "";
+      if (data.items && data.items.length > 0) {
+        title = data.items[0].snippet.title;
+      }
+      config.callback(title, player);
+    });
+  }
+
+  function checkIsLive(liveId, callback) {
+    var checkIsLiveUrl = "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + liveId + "&key=AIzaSyBoMXQDrlRUCQCxv4fjfiyTHXog8OB2Nz0";
+    httpGet(checkIsLiveUrl, function (data) {
+      callback(data.items && data.items.length > 0 && data.items[0].snippet.liveBroadcastContent !== "none");
+    });
+  }
+
+  function pollEventsApi() {
+    httpGet(config.liveIdUrl, function(ret) {
+      if (ret.id !== lastLiveId) {
+        if (ret.id) {
+          checkIsLive(ret.id, function(isLive) {
+            if (isLive) {
+              lastLiveId = ret.id;
+              showSplash();
+              loadYouTubePlayer(ret.id);
+            } else {
+              showSplash();
+            }
+          });
+        } else {
+          checkIsLive(lastLiveId, function(isLive) {
+            if (!isLive) {
+              lastLiveId = null;
+              showSplash();
+            }
+          });
+        }
+      }
+    });
+  }
+
+  function loadYouTubeAPI() {
+    var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  }
+
+  function httpGet(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+      if (xmlHttp.readyState === 4) {
+        callback(JSON.parse(xmlHttp.responseText));
+      }
+    };
+    xmlHttp.open("GET", theUrl, true);
+    xmlHttp.send();
+  }
+
+})();
