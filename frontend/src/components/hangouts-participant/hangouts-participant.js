0c54b764f9cd42078bf76428be465759ca4c33e3
kolmanv <kolmanv@gmail.com>
2015-06-21 22:53:31 -0400

    Numbers fixed

--- frontend/src/components/hangouts-participant/hangouts-participant.js
+++ frontend/src/components/hangouts-participant/hangouts-participant.js
@@ -24,6 +24,10 @@
       var that = this;
 
       gapi.hangout.onParticipantsChanged.add(function () {
+        that.updateParticipantsNumbers();
+      });
+
+      gapi.hangout.data.onStateChanged.add(function() {
         that.initOverlay();
       });
 
@@ -51,18 +55,67 @@
     },
     initOverlay: function () {
       var that = this;
-      var ids = this.getParticipantsIds();
+      var state = gapi.hangout.data.getState();
 
-      ids.forEach(function (id, index) {
-        var imagePath = that.$.config.numbersImagePath.replace('{}', index + 1);
-        gapi.hangout.av.setAvatar(id, that.$.config.frontendUrl + imagePath);
-      });
+      for (var id in state) {
+        if (state.hasOwnProperty(id)) {
+          var index = state[id];
+          var imagePath = that.$.config.numbersImagePath.replace('{}', index);
+          gapi.hangout.av.setAvatar(id, that.$.config.frontendUrl + imagePath);
+        }
+      }
     },
     mute: function () {
       gapi.hangout.av.setMicrophoneMute(true);
     },
     hide: function () {
       gapi.hangout.hideApp();
+    },
+    updateParticipantsNumbers: function() {
+      if (this.shouldIUpdate()) {
+        var ids = this.getParticipantsIds();
+        var state = gapi.hangout.data.getState();
+        var keysToRemove = [];
+
+        // Free numbers
+        for (var key in state) {
+          if (state.hasOwnProperty(key)) {
+            if (ids.indexOf(key) === -1) {
+              delete state[key];
+              keysToRemove.push(key);
+            }
+          }
+        }
+
+        // Assign new numbers
+        ids.forEach(function(id) {
+          if (state[id] === undefined) {
+            var freeNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+            for (var key in state) {
+              if (state.hasOwnProperty(key)) {
+                var value = state[key];
+                var index = freeNumbers.indexOf(value);
+                if (index !== -1) {
+                  freeNumbers.splice(index, 1);
+                }
+              }
+            }
+            state[id] = freeNumbers[0];
+          }
+        });
+
+        gapi.hangout.data.submitDelta(state, keysToRemove);
+      }
+    },
+    shouldIUpdate: function() {
+      var hangoutParticipants = gapi.hangout.getEnabledParticipants();
+      var minId = null;
+      hangoutParticipants.forEach(function (participant) {
+        if (minId === null || participant.id < minId) {
+          minId = participant.id;
+        }
+      });
+      return gapi.hangout.getLocalParticipantId() === minId;
     },
     getParticipantsIds: function () {
       var hangoutParticipants = gapi.hangout.getParticipants();
